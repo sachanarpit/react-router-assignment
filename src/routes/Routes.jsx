9014dc5d82915ca14d09cd80ce9cb8c7e2659c43
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-18 23:50:20 +0530

    routes are written

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+
+export const Routes = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+
+        <Route path="/products">
+          <h1>Products</h1>
+        </Route>
+
+        <Route path="/products/:id">
+          <h1>Single Product</h1>
+        </Route>
+        <Route>
+          <h1>Page not found</h1>
+        </Route>
+      </Switch>
+    </>
+  );
+};
